5a5b2b5525cce5e1bc1be7abc0a7f11c64ccc8d4
Aimilios <aimilios.tsiaprazis@its-stuttgart.de>
2025-06-06 13:58:45 +0200

    Repair

--- obj/Debug/net9.0/SwaggerProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/SwaggerProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SwaggerProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74ef48dc936b76c614fd40df11e8fa404ec27e49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41563689afea956f97bfb12d1a0593730faad8a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SwaggerProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SwaggerProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
