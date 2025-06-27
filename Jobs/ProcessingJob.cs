641880fccc282ac2219acf1eac8f23cc8d9757c4
Aimilios <aimilios.tsiaprazis@its-stuttgart.de>
2025-06-27 10:39:51 +0200

    Creating and Programming Hangfire Job Service

---
+++ Jobs/ProcessingJob.cs
@@ -0,0 +1,23 @@
+using System;
+using Hangfire;
+using Hangfire.MemoryStorage;
+
+public class ProcessingJob
+{
+    /// A job that simply prints a line to the console when it runs.
+    public async Task ConsoleVerification()
+    {
+        System.Console.WriteLine("Hangfire Processing Job is working!");
+    }
+    public async Task DeveloperVerification()
+    {
+        string name = "Aimilios Tsiaprazis";
+        int age = 22;
+        string email = "a.tsiaprazis@digital-worx.de";
+        string job = "Software Developer";
+        System.Console.WriteLine(name);
+        System.Console.WriteLine(age);
+        System.Console.WriteLine(email);
+        System.Console.WriteLine(job);
+    }
+}
